be92afe72eb9fbfd7d045533295f6cccfbead8ea
Chamika196 <chamikarathnayaka1996@gmail.com>
2022-12-16 15:04:40 +0530

    Setup React Client

---
+++ src/components/avenger.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from "react"
+
+class Avenger extends Component{
+    render(){
+        return <h1>Avenger Incoming - Website in Progress</h1>
+    }
+}
+
+export default Avenger;
